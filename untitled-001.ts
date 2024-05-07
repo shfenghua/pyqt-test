55dabd35dfb10d21b4cf1c9dcad1c5ac66bad735
shfenghua <35173434@qq.com>
2024-05-07 10:28:06 +0800

    initial commit

---
+++ untitled-001.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="untitled-001.py" line="54"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="untitled-001.py" line="55"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="untitled-001.py" line="56"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
